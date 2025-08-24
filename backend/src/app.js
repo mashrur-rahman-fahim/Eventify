833a82558c293003e33ef49295a179636baa20af
mashrur-rahman-fahim <mashrur950@gmail.com>
2025-08-24 18:29:43 +0600

    fixed deployed issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -87,8 +87,8 @@ if (process.env.NODE_ENV === "production") {
   });
 
   // Handle all other routes by serving the React app
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
